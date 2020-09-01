3edef49d99e3153481eff9b6fc6730dae7bde561
Mohammad Amin Abdallah Obeidat <MohammadO>
2020-09-01 15:08:09 +0300

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+'name': 'Library Management',
+'description': 'Manage library book catalogue and lending.',
+'author': 'M. Obeidat',
+'depends': ['base'],
+'application': True,
+'data':['security/library_security.xml','security/ir.model.access.csv','views/library_menu.xml','views/book_view.xml','views/book_list_template.xml',],
+}
